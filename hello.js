72886b7d45b3dc05db9e093d6e1e96dd3b84c32f
Varun Jatav <varunjatav.kv@gmail.com>
2025-12-21 21:05:13 +0530

    added two files

---
+++ hello.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(x, y) {
+    return x + y;
+}
+;
+const result = add(5, 10);
+console.log(result);
---
+++ hello.ts
@@ -0,0 +1,6 @@
+function add (x: number, y: number){
+    return x+y;
+};
+const result:number = add(5,10);
+console.log(result);
+
