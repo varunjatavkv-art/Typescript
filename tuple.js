2fa808895fa5983d7df85e53bf51351d91a75d6c
Varun Jatav <varunjatav.kv@gmail.com>
2025-12-25 12:43:15 +0530

    tuple concept of type script is completed

---
+++ tuple.js
@@ -0,0 +1,3 @@
+var ourTuple;
+ourTuple = [5, "Coding God was here", true];
+console.log(ourTuple);
---
+++ tuple.ts
@@ -0,0 +1,3 @@
+let ourTuple: [number,string,boolean];
+ourTuple = [5,"Coding God was here", true];
+console.log(ourTuple);
